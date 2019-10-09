94c41f6e3729e281bfe146f08fc6124c9a9cec7d
Alexandre Rodrigues <alexandre@kivra.com>
2019-10-09 16:38:00 +0200

    Clean up supervisor code

--- src/id_token_sup.erl
+++ src/id_token_sup.erl
@@ -16,20 +16,15 @@
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-%% sup_flags() = #{strategy => strategy(),         % optional
-%%                 intensity => non_neg_integer(), % optional
-%%                 period => pos_integer()}        % optional
-%% child_spec() = #{id => child_id(),       % mandatory
-%%                  start => mfargs(),      % mandatory
-%%                  restart => restart(),   % optional
-%%                  shutdown => shutdown(), % optional
-%%                  type => worker(),       % optional
-%%                  modules => modules()}   % optional
 init([]) ->
-    SupFlags = #{strategy => one_for_all,
-                 intensity => 0,
-                 period => 1},
-    ChildSpecs = [],
-    {ok, {SupFlags, ChildSpecs}}.
+  SupFlags = #{strategy => one_for_one, intensity => 10, period => 1},
 
-%% internal functions
+  ChildSpecs = [#{id => id_token,
+                 start => {id_token, start_link, []},
+                 restart => permanent,
+                 shutdown => brutal_kill,
+                 type => worker,
+                 modules => [id_token]
+                }
+               ],
+  {ok, {SupFlags, ChildSpecs}}.
