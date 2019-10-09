fb02ac6ce98df07589acff3b47b6a2dddda47231
Alexandre Rodrigues <alexandre@kivra.com>
2019-10-09 16:37:00 +0200

    get_jwk_uri/1 just needs the Uri

--- src/id_token_jwks.erl
+++ src/id_token_jwks.erl
@@ -6,7 +6,9 @@
         ]).
 
 -type keys() :: #{exp_at := integer(), keys := list()}.
--type provider() :: #{name := atom(), well_known_uri := binary()}.
+-type provider() :: {atom(), binary()}.
+
+-export_type([keys/0, provider/0]).
 
 -spec get_pub_keys(binary()) -> keys() | {error, any()}.
 get_pub_keys(Uri) ->
@@ -26,8 +28,9 @@ get_pub_keys(Uri) ->
       {error, Reason}
   end.
 
--spec get_jwks_uri(provider()) -> {ok, binary()} | {error, any()}.
-get_jwks_uri(#{well_known_uri := Uri}) ->
+%% Returns the jwks_uri given the well-known open id connect configuration URI
+-spec get_jwks_uri(binary()) -> {ok, binary()} | {error, any()}.
+get_jwks_uri(Uri) ->
   case hackney:request(get, Uri, [], <<>>, [with_body]) of
     {ok, 200, _Headers, Body} ->
       #{<<"jwks_uri">> := JwksUri} = jsx:decode(Body, [return_maps]),
@@ -41,7 +44,6 @@ get_jwks_uri(#{well_known_uri := Uri}) ->
 -spec get_providers() -> [provider()].
 get_providers() ->
   application:get_env(id_token, providers, []).
-
 
 %%%_* Emacs ============================================================
 %%% Local Variables:
