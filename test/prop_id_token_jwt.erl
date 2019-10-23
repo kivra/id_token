549b85f0fa71f66d15e758bd360df28b7e931c0a
Alexandre Rodrigues <alexandre@kivra.com>
2019-10-23 14:50:34 +0200

    Introduce id_token_test_util

--- test/prop_id_token_jwt.erl
+++ test/prop_id_token_jwt.erl
@@ -6,7 +6,6 @@
 
 %Higher sizes are too slow
 -define(MODULUS_SIZES, [1024]).
--define(EXPONENT_SIZE, 65537).
 
 %%%%%%%%%%%%%%%%%%
 %%% Properties %%%
@@ -16,7 +15,7 @@ prop_valid_signature() ->
           {rsa_key_pair(), jwt_claims()},
           begin
             #{<<"exp">> := Exp} = Claims,
-            Jwt = sign(Jwk, Claims),
+            Jwt = id_token_test_util:sign(Jwk, Claims),
             Result = id_token_jwt:validate(Jwt, [PublicKeyMap]),
             Exp =<  erlang:system_time(second)
               andalso {error, expired} =:= Result
@@ -29,7 +28,7 @@ prop_invalid_signature() ->
           begin
             #jose_jwk{fields = OtherFields} = OtherJwk,
             JwkWithChangedKid = Jwk#jose_jwk{fields = OtherFields},
-            Jwt = sign(JwkWithChangedKid, Claims),
+            Jwt = id_token_test_util:sign(JwkWithChangedKid, Claims),
             {error, invalid_signature}
               =:= id_token_jwt:validate(Jwt, [OtherPublicKeyMap])
           end).
@@ -38,23 +37,11 @@ prop_no_matching_key() ->
   ?FORALL({[{Jwk, _PublicKeyMap} | OtherKeys], Claims},
           {non_empty(list(rsa_key_pair())), jwt_claims()},
           begin
-            Jwt = sign(Jwk, Claims),
+            Jwt = id_token_test_util:sign(Jwk, Claims),
             PublicKeys = lists:map(fun({_, Key}) -> Key end, OtherKeys),
             {error, no_public_key_matches}
               =:= id_token_jwt:validate(Jwt, PublicKeys)
           end).
-
-%%%%%%%%%%%%%%%
-%%% Helpers %%%
-%%%%%%%%%%%%%%%
-
-sign(#jose_jwk{fields = #{<<"kid">> := Kid}} = Jwk, Claims) ->
-  JWS = #{
-          <<"alg">> => <<"PS256">>,
-          <<"kid">> => Kid
-         },
-  Jwt = jose_jwt:sign(Jwk, JWS, Claims),
-  jose_jws:compact(Jwt).
 
 %%%%%%%%%%%%%%%%%%
 %%% Generators %%%
@@ -67,15 +54,9 @@ jwt_claims() ->
        BaseMap#{<<"aud">> => Aud, <<"exp">> => Exp}).
 
 rsa_key_pair() ->
-  ?LET(KeySize, oneof(?MODULUS_SIZES),
-       begin
-         RsaKey = public_key:generate_key({rsa, KeySize, ?EXPONENT_SIZE}),
-         Jwk0 = jose_jwk:from_key(RsaKey),
-         Kid = base64url:encode(crypto:strong_rand_bytes(16)),
-         Jwk = Jwk0#jose_jwk{fields = #{<<"kid">> => Kid}},
-         {_, PublicKeyMap} = jose_jwk:to_public_map(Jwk),
-         {Jwk, PublicKeyMap}
-       end).
+  ?LET(KeySize,
+       oneof(?MODULUS_SIZES),
+       id_token_test_util:generate_rsa_key_pair(KeySize)).
 
 %%%_* Emacs ============================================================
 %%% Local Variables:
