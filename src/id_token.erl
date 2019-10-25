6b184f3c57db043e438ee4539facaa76e7795b87
Alexandre Rodrigues <alexandre@kivra.com>
2019-10-25 16:52:11 +0200

    Remove optional callbacks

--- src/id_token.erl
+++ src/id_token.erl
@@ -7,8 +7,7 @@
 -export([start_link/0, validate/2]).
 
 %% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3, format_status/2]).
+-export([init/1, handle_call/3, handle_cast/2]).
 
 -define(SERVER, id_token_server).
 -define(ID_TOKEN_CACHE, id_token_cache).
@@ -99,47 +98,6 @@ handle_call({refresh, Provider}, _From, State) ->
 %%--------------------------------------------------------------------
 handle_cast(_Request, State) ->
   {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%% @end
-%%--------------------------------------------------------------------
-handle_info(_Info, State) ->
-  {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%% @end
-%%--------------------------------------------------------------------
-terminate(_Reason, _State) ->
-  ok.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%% @end
-%%--------------------------------------------------------------------
-code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called for changing the form and appearance
-%% of gen_server status when it is returned from sys:get_status/1,2
-%% or when it appears in termination error logs.
-%% @end
-%%--------------------------------------------------------------------
-format_status(_Opt, Status) ->
-  Status.
 
 %%%===================================================================
 %%% Internal functions
